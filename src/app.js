339393387add53ea20e4b3b9757cce1176fd3602
Ed Soliman <e.georgesoliman@gmail.com>
2019-09-23 12:49:06 -0400

    server udpate

--- src/app.js
+++ src/app.js
@@ -9,66 +9,19 @@ const addWorkoutsRouter = require('./Add Workouts/addworkouts-router')
 const deleteWorkoutsRouter = require('./deleteWorkouts/deleteWorkouts-router')
 
 const app = express()
-
 const morganOption = (NODE_ENV === 'production')
 	? 'tiny'
 	: 'common';
 
+	
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-// 1) all fields required - must validate exist
-// 2) 
+
 app.use('/viewworkouts', viewWorkoutsRouter, deleteWorkoutsRouter);
 app.use('/addworkouts', addWorkoutsRouter);
-//app.use("/api/workouts", workoutsRouter);
 
-
-
-
-// 1) all fields required - must validate exist
-// 2) 
-
-// app.post('/addworkout', (req, res, next) => {
-// 	// field boolean validation (all field)
-// 	for (const field of ['bodyPart', 'exercise,', 'sets', 'reps']) {
-// 		if(!req.body[field]) {
-// 			return res.status(400).send(`${field} is required.`)
-// 		}
-// 	}
-
-// 	// text string validation (body part, exercise)
-// 	for (const field of ['bodypart', 'exercise']) {
-// 		if(!typeof req.body[field] == 'string') {
-// 			return res.status(400).send(`${field} must be a text value.`)
-// 		}
-// 	}
-
-// 	// is a number validation (sets, reps)
-// 	for (const field of ['sets', 'reps', 'weight']) {
-// 		if(!typeof req.body[field] == 'string') {
-// 			return res.status(400).send(`${field} must be an integer.`)
-// 		}
-// 	}
-
-// 	const { bodyPart, exercise, sets, reps, weight} = req.body
-// 	const newWorkout = { bodyPart, exercise, sets, reps, weight }
-
-// 	workoutService.insertWorkout(
-// 		req.app.get('db'),
-// 		newWorkout
-// 	)
-// 		.then(workout => {
-// 			res
-// 				.status(201)
-// 				.json(workout)
-// 		})
-// 		.catch(next)
-
-	
-    
-// });
 
 app.use(function errorHandler(error, req, res, next) {
 	let response
