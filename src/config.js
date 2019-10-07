5dd532af7386fb350318cbabf47db5af2fe26419
Ed Soliman <e.georgesoliman@gmail.com>
2019-10-07 15:10:03 -0400

    db updates

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     API_TOKEN: process.env.API_TOKEN
-    // DB_URL: process.env.DB_URL || 'postgres://postgres:postgres@localhost/workouts',
     // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
   }
